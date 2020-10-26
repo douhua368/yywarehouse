12390eaf3f08a5afb8d68108ce082859ef7e439c
douhua <2212769486@qq.com>
2020-10-26 14:28:16 +0800

    week_0402

---
+++ week_0402/maxandmin.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<limits.h>
+#include<float.h>
+int main(){
+	printf( "INT min=%d , INT max=%d\n " ,INT_MIN,INT_MAX);
+	printf("FLOAT pin=%f ,FLOAT max=%f\n" ,FLT_MIN,FLT_MAX);
+	printf("DOUBLE min=%f ,DOUBLE max=%f\n " ,DBL_MIN,DBL_MAX);
+	return 0;
+ }
